2a0ba08dae8dd7854699d74743ee45d965b40ff9
Harsh <haarshkumaar@gmail.com>
2025-12-27 01:32:26 +0530

    Update Auth

--- controller/userController.js
+++ controller/userController.js
@@ -1,28 +1,75 @@
-const User = require("../model/user")
+const User =require("../model/user")
+const bcrypt = require("bcrypt")
+const disposableEmailDomains = require("disposable-email-domains")
 
-const loginUser = async(req,resp) => {
-    try {
+const loginUser = async(req,resp)=>{
+     try {
         const {email,password} = req.body
         if(!email || !password){
-            resp.send({message:"Email and Password are required"})
+            resp.status(404).send({message:"Email and Password are required"})
             return
         }
         if(typeof email !== "string" || typeof password !== "string"){
-            resp.send({message:"Invalid Email or Password"})
+            resp.status(400).send({message:"Invalid Email or Password"})
             return
         }
-        const existingUser = await User.findOne({email})
+        
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(!emailRegex.test(email)){
+            resp.status(400).send({message:"Invalid Email Format"})   
+            return
+        }
+
+        const passwordStrengthRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+        if(!passwordStrengthRegex.test(password)){
+            resp.status(400).send({message:"Password should have one uppercase, lowercase, symbol and number"})
+            return
+        }
+
+        const domain = email.split("@")[1]
+        if(disposableEmailDomains.includes(domain)){
+            resp.status(400).send({message:"Spam Email found. Invalid Email"})
+            return
+        }
+
+        const updatedEmail = email.trim().toLowerCase()
+        const existingUser = await User.findOne({email:updatedEmail})
         if(existingUser){
-            resp.send({message:"User with this email already exists"})   
+            if(!existingUser.password){
+                resp.status(400).send({message:"You have loggedIn with your google account"})
+                return
+            }
+
+            const isMatched = await bcrypt.compare(password,existingUser.password)
+            if(!isMatched){
+                resp.status(400).send({message:"Invalid Credentials"})
+            }
+
+            if(!existingUser.verified){
+                // otp to email
+            }
+
+            if(!existingUser.service){
+                resp.status(400).send({message:"Your service has been disbled. Contact website support"})
+                return
+            }
+
+            resp.status(200).send({message:"Login successfully"})   
             return
         }
-        const result = await User.create({email,password})
-        resp.send({message:"User created successfully",data:result});
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt)
+
+        const result = await User.create({updatedEmail,hashedPassword})
+        resp.status(201).send({message:"Login successfully",data:result});
     } catch (error) {
-        resp.send({message:"Internal Server Error",error})
+        resp.status(500).send({message:"Internal Server Error",error})
     }    
 }
-const verifyUser = (req,resp) => {
 
+const verifyUser = (req,res)=>{
+    
 }
-module.exports = {loginUser,verifyUser}
+
+module.exports ={loginUser ,verifyUser}
